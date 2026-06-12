dffea927c137d90ade31b1ccc88243d11ef5cbfe
r1zuuu <stanislawrayzacher2@gmail.com>
2026-06-12 16:53:52 +0200

    feature: Added transactions/[id] -> DELETE, and transactions -> POST

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server"
 import { db } from "@/db"
 import { eq, and } from "drizzle-orm"
 import { transactions, wallets } from "@/db/schema"
+import { type InferInsertModel } from "drizzle-orm" // drizzle orm provides a type for insert for each table
 
 export async function GET(request: Request) {
     const session = await auth.api.getSession({
@@ -33,4 +34,35 @@ export async function GET(request: Request) {
         where: eq(transactions.walletId, walletId),
     })
     return NextResponse.json(userTransactions, { status: 200 })
-} 
+}
+
+export async function POST(request: Request) {
+    const session = await auth.api.getSession({
+        headers: request.headers // reading the session from cookies
+    })
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+    }
+    const { amount, type, category, description, walletId } = await request.json()
+
+    const isUserWallet = await db.query.wallets.findFirst({
+        where: and(
+            eq(wallets.id, walletId),
+            eq(wallets.userId, session.user.id)
+        )
+    })
+    
+    if (!isUserWallet) {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 })
+    }
+
+    type NewTransaction = InferInsertModel<typeof transactions>
+
+
+    const newTransaction = await db.insert(transactions).values({ amount, type, category, description, walletId }).returning()
+
+    return NextResponse.json(newTransaction, { status: 200 })
+
+
+
+}
